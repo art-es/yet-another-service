0c75967ce9f9bc0eb4c2d2167849280bea919761
Artur Shaidullin <art-es>
2024-12-18 14:19:25 +0300

    change token black list key method usage

--- internal/storage/redis/auth_token_black_list.go
+++ internal/storage/redis/auth_token_black_list.go
@@ -20,7 +20,7 @@ func NewAuthTokenBlackListStorage(db *redis.Client) *AuthTokenBlackListStorage {
 }
 
 func (s *AuthTokenBlackListStorage) Add(ctx context.Context, token string, ttl time.Duration) error {
-	err := s.db.Set(ctx, tokenToKey(token), "1", ttl).Err()
+	err := s.db.Set(ctx, s.key(token), "1", ttl).Err()
 	if err != nil {
 		return fmt.Errorf("execute command: %w", err)
 	}
@@ -29,7 +29,7 @@ func (s *AuthTokenBlackListStorage) Add(ctx context.Context, token string, ttl t
 }
 
 func (s *AuthTokenBlackListStorage) Has(ctx context.Context, token string) (bool, error) {
-	if err := s.db.Get(ctx, tokenToKey(token)).Err(); err != nil {
+	if err := s.db.Get(ctx, s.key(token)).Err(); err != nil {
 		if err == redis.Nil {
 			return false, nil
 		}
@@ -40,6 +40,6 @@ func (s *AuthTokenBlackListStorage) Has(ctx context.Context, token string) (bool
 	return true, nil
 }
 
-func tokenToKey(token string) string {
+func (s *AuthTokenBlackListStorage) key(token string) string {
 	return fmt.Sprintf("authtokenblacklist_%x", sha256.Sum256([]byte(token)))
 }
